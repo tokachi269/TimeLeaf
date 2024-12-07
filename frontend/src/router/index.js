093c47e87bafc17e917b28330b0c5d4fac88a2b4
tokachi269 <39192302+tokachi269@users.noreply.github.com>
2024-12-07 22:48:44 +0900

    createWebHashHistory

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,5 @@
 import App from '../App.vue';
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 
 const routes = [
     {
@@ -10,7 +10,7 @@ const routes = [
   ];
   
   const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(),
     routes,
   });
   
