b0552c79d17be1eb4acf3c596237660e221fc62f
tokachi269 <39192302+tokachi269@users.noreply.github.com>
2024-12-10 07:22:02 +0900

    a

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,5 @@
 import App from '../App.vue';
 import { createRouter, createWebHistory } from 'vue-router';
-import NotFound from './components/NotFound.vue'; //追記
 
 const routes = [
   {
@@ -9,16 +8,11 @@ const routes = [
     code: 'code',
     component: App,
     meta: { title: 'TimeLeaf' }
-  },
-  {
-    path: '/:pathMatch(.*)*', //ここから3行追記
-    name: 'NotFound',
-    component: NotFound
-  },
+  }
 ];
 
 const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
+  history: createWebHistory("/"),
   routes,
 });
 
