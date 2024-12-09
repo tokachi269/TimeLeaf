3f9095888bcec4966d7c7dcbffea83ab8073c314
tokachi269 <39192302+tokachi269@users.noreply.github.com>
2024-12-10 07:04:49 +0900

    route

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,19 +1,26 @@
 import App from '../App.vue';
 import { createRouter, createWebHashHistory } from 'vue-router';
+import NotFound from './components/NotFound.vue' //追記
 
 const routes = [
-    {
-      path: '/', // ルートパスに設定
-      name: 'TimeLeaf',
-      code: 'code',
-      component: App,
-      meta: { title: 'TimeLeaf' }
-    },
-  ];
-  
-  const router = createRouter({
-    history: createWebHashHistory("/"),
-    routes,
-  });
-  
-  export default router;
+  {
+    path: '/', // ルートパスに設定
+    name: 'TimeLeaf',
+    code: 'code',
+    component: App,
+    meta: { title: 'TimeLeaf' }
+
+  }, {
+    path: '*', //ここから3行追記
+    name: 'NotFound',
+    component: NotFound
+  },
+];
+
+const router = createRouter({
+  mode: "history",
+  history: createWebHashHistory("/"),
+  routes,
+});
+
+export default router;
