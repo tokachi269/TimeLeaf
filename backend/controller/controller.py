b5249c49f8a2c3cda8bea4d4d2b8c3c84d7228fc
tokachi269 <39192302+tokachi269@users.noreply.github.com>
2024-12-10 21:40:37 +0900

    headers

--- backend/controller/controller.py
+++ backend/controller/controller.py
@@ -222,6 +222,7 @@ def get_slack_reactions():
     headers = {
         "Authorization": f"Bearer {token}",
     }
+    print(headers)
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
         res = response.json() 
