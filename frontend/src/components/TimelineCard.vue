37aa538251fede3062dede274d967b4c19bebc4b
tokachi269 <39192302+tokachi269@users.noreply.github.com>
2024-12-20 23:47:35 +0900

    album

--- frontend/src/components/TimelineCard.vue
+++ frontend/src/components/TimelineCard.vue
@@ -273,7 +273,7 @@ export default {
           if (trackId) {
             // サムネイル用HTMLを作成
             this.thumbnailHtml = `
-                <iframe style="border-radius:12px" src="https://open.spotify.com/embed/${url.includes("track") ? "track" : url.includes("album") ? "track" : "playlist"}/${trackId}?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+                <iframe style="border-radius:12px" src="https://open.spotify.com/embed/${url.includes("track") ? "track" : url.includes("album") ? "album" : "playlist"}/${trackId}?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
               `;
           }
         } else if (url.includes("youtu.be") || url.includes("youtube.com")) {
