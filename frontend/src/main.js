d698ca99e5cb71b8415a8031de8e4a392fd66732
tokachi269 <39192302+tokachi269@users.noreply.github.com>
2024-12-20 23:44:02 +0900

    c

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,22 +1,10 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router/index.js'; // ルーターをインポート
-import { VPullToRefresh } from 'vuetify/labs/VPullToRefresh';
-import * as components from 'vuetify/components';
-import * as directives from 'vuetify/directives';
-import { createVuetify } from 'vuetify';
 
 // Vueアプリケーションを作成
 const app = createApp(App);
-const vuetify = createVuetify({
-    components: {
-      ...components,
-      VPullToRefresh
-    },
-    directives,
-  });
-  
- app.use(vuetify);
+
 // ルーターを登録
 app.use(router);
 
